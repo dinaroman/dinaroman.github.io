bf66c060ec716ead60c652b99ef387a9b38c1589
dinaroman <144297253+dinaroman@users.noreply.github.com>
2023-09-11 11:09:59 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,69 +2,74 @@ const CALCULAR = document.getElementById('calcular');
 const ERROR = document.getElementById('error');
 const FLU = document.getElementById('flu');
 const MAN = document.getElementById('man');
-const HOLLIDAYSEGAR = document.getElementById('calculo1');
-const SC = document.getElementById('calculo2');
-const SC1 = document.getElementById('sc1');
-const SC2 = document.getElementById('sc2');
+const SC1500 = document.getElementById('sc1');
+const SC2000 = document.getElementById('sc2');
+const SC = document.getElementById('sc');
+
 
 function calcFlujo(peso){
-    let resto = peso;
-    let flujo = 0;
-    if (resto > 20){
-        let aux = resto - 20;
-        flujo += aux * 1;
-        resto -= aux;
+
+    let pesoAuxiliar = peso;
+    let mantenimiento = 0;
+    
+    if (pesoAuxiliar>20){
+        let aux = pesoAuxiliar-20;
+        mantenimiento += aux*20;
+        pesoAuxiliar -= aux;
     } 
-    if (resto > 10){
-        let aux = resto - 10;
-        flujo += aux * 2;
-        resto -= aux;
-    }
-    flujo += resto * 4;
+    if (pesoAuxiliar>10){
+        let aux = pesoAuxiliar-10;
+        mantenimiento += aux*50;
+        pesoAuxiliar -= aux;
+    }  
+    mantenimiento += pesoAuxiliar*100;
+
+    let flujo = mantenimiento / 24
+    flujo = Math.ceil(flujo)
+
     return flujo;
 }
 
 function calcSuperficieCorporal(peso){
     peso = parseFloat(peso);
-    let calculo = ((peso * 4) + 7) / (peso + 90);
-    return calculo;
+    calculo = ((peso * 4) + 7) / (peso + 90)
+    return calculo
 }
 
 CALCULAR.addEventListener('click', () => {
-    const DATO = document.getElementById('peso').value;
-    // Validamos que se cargue un dato:
+    const DATO = document.getElementById('peso').value
+    
+    //validamos que se cargue un dato:
+
     if (DATO > 0 && DATO <= 30){
-        ERROR.style.display = 'none';
+        ERROR.style.display = 'none'
+
         let flujo = calcFlujo(DATO);
-        let mantenimiento = flujo * 1.5;
-
-        HOLLIDAYSEGAR.innerHTML = 'Holliday-Segar';
-        FLU.innerHTML = 'Mantenimiento = ' + flujo + ' cc/hr';
-        MAN.innerHTML = 'm+m/2 = ' + mantenimiento + ' cc/hr';
+        let mantenimiento = flujo*1.5;
+        FLU.innerHTML = 'Flujo = ' + flujo + ' cc/hr';
+        MAN.innerHTML = 'm+m/ 2 =' + mantenimiento + ' cc/hr';
         FLU.style.display = 'block';
         MAN.style.display = 'block';
-    } else if (DATO > 30){
-        ERROR.style.display = 'none';
 
-        document.getElementById('resultado1').style.display = 'none';
-        document.getElementById('resultado2').style.display = 'none';
+    } else if(DATO > 30){
+        ERROR.style.display = 'none'
 
-        let superficieCorporal = calcSuperficieCorporal(DATO);
-        let superficieCorp1500 = (superficieCorporal * 1500).toFixed(2);
-        let superficieCorp2000 = (superficieCorporal * 2000).toFixed(2);
+        let superficieCorporalNormal = calcSuperficieCorporal(DATO);
+        console.log(superficieCorporalNormal);
+        let superficieCorporal1500 = superficieCorporalNormal * 1500;
+        superficieCorporal1500 = superficieCorporal1500.toFixed(2);
+        let superficieCorporal2000 = superficieCorporalNormal * 2000;
+        superficieCorporal2000 = superficieCorporal2000.toFixed(2);
 
-        SC.innerHTML = 'Superficie Corporal';
-        SC1.innerHTML = 'Superficie Corporal x 1500 = ' + superficieCorp1500;
-        SC2.innerHTML = 'Superficie Corporal x 2000 = ' + superficieCorp2000;
+        SC1500.innerHTML = 'Superficie Corporal x 1500 = ' + superficieCorporal1500;
+        SC2000.innerHTML = 'Superficie Corporal x 2000 = ' + superficieCorporal2000;
         
-        SC.style.display = 'block';
         FLU.style.display = 'block';
         MAN.style.display = 'block';
 
-        document.getElementById('resultado2').style.display = 'block';
-    } else {
+    }else{
         ERROR.style.display = 'block';
         FLU.style.display = 'none';
         MAN.style.display = 'none';
     }
-});
+})
